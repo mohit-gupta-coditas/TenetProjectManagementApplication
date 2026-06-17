71a806c00e57b97006eb9041dbc43ae4e004ec4c
Ananthapadmanabhan <ananthapadmanabhan.s.s@coditas.com>
2026-06-17 08:50:38 +0530

    Updated Generic Button

--- frontend/src/Components/GenericComponents/Button/Button.tsx
+++ frontend/src/Components/GenericComponents/Button/Button.tsx
@@ -1,10 +1,22 @@
+import clsx from 'clsx';
 import styles from './Button.module.scss'
 import type { ButtonProps } from './Button.types';
 
-const Button=({className,children,onClick,...props}:ButtonProps)=>{
+const Button=({className,children,...props}:ButtonProps)=>{
   return(
-    <button {...props} onClick={onClick} className={[styles.button,className].join(" ")}>{children}</button>
+    <button {...props} className={clsx(styles.button,className)}>{children}</button>
   );
+}
+export const PrimaryButton = (props: ButtonProps) => {
+    return <Button {...props} className={styles.PrimaryButton}></Button>
+}
+
+export const SecondaryButton = (props: ButtonProps) => {
+    return <Button {...props} className={styles.SecondaryButton}></Button>
+}
+
+export const TertiaryButton = (props: ButtonProps) => {
+    return <Button {...props} className={styles.TertiaryButton}></Button>
 }
 
 export default Button
